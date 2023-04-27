62735824cba923fee94631cbfa6ef13a4f04948e
lucianatux <lucianatux@yahoo.com.ar>
2023-04-27 13:46:00 -0300

    marked y estilos

--- markdownPreviewer/script.js
+++ markdownPreviewer/script.js
@@ -1,7 +1,12 @@
+
 const editor = document.getElementById('editor');
 const preview = document.getElementById('preview');
 
-function updatePreview() {
+  function updatePreview() {
     preview.innerHTML = editor.value;
+    preview.innerHTML = marked(editor.value); 
   }
+  
   editor.addEventListener('input', updatePreview);
+  
+  preview.innerHTML = marked(editor.value); // Mostrar el contenido de muestra al cargar la página
