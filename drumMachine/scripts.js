e514bba8961d0e25f5b4be80050e6fcc45aeef41
lucianatux <lucianatux@yahoo.com.ar>
2023-05-02 18:27:30 -0300

    funciona teclas

--- drumMachine/scripts.js
+++ drumMachine/scripts.js
@@ -1,29 +1,54 @@
 // Get all the drum pad elements
-const drumPads = document.querySelectorAll('.drum-pad');
+const drumPads = document.querySelectorAll(".drum-pad");
 
 // Add a click event listener to each drum pad
-drumPads.forEach(pad => {
-  pad.addEventListener('click', () => {
+drumPads.forEach((pad) => {
+  pad.addEventListener("click", () => {
     // Get the audio element within the drum pad
-    const audio = pad.querySelector('.clip');
+    const audio = pad.querySelector(".clip");
     // Play the audio clip
     audio.play();
   });
 });
 
-document.addEventListener('keydown', event => {
-    // Get the pressed key
-    const key = event.key.toUpperCase();
-    console.log(key);
-    console.log(drumPad);
-    // Find the drum pad with the matching id
-    const drumPad = document.getElementById(key);
-    // If a matching drum pad is found, trigger its audio clip
-    if (drumPad) {
-      const audio = drumPad.querySelector('.clip');
-      audio.currentTime = 0;
-      audio.play();
-    }
-  });
-  
-  
+function playSound(key) {
+  var audio = document.getElementById(key);
+  audio.currentTime = 0; // Reiniciar la reproducción del audio si ya está sonando
+  audio.play();
+}
+
+// Detectar cuando se presiona una tecla
+document.addEventListener("keydown", function (event) {
+  switch (event.key.toUpperCase()) {
+    case "Q":
+      playSound("Q");
+      break;
+    case "W":
+      playSound("W");
+      break;
+    case "E":
+      playSound("E");
+      break;
+    case "A":
+      playSound("A");
+      break;
+    case "S":
+      playSound("S");
+      break;
+    case "D":
+      playSound("D");
+      break;
+    case "Z":
+      playSound("Z");
+      break;
+    case "X":
+      playSound("X");
+      break;
+    case "C":
+      playSound("C");
+      break;
+    default:
+      // No hacer nada si se presiona otra tecla
+      break;
+  }
+});
