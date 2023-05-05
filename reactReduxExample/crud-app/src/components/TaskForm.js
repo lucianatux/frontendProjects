720b40aa2593585b3355df5158a5c44409c7de02
lucianatux <lucianatux@yahoo.com.ar>
2023-05-05 20:29:58 -0300

    Update TaskForm.js

--- reactReduxExample/crud-app/src/components/TaskForm.js
+++ reactReduxExample/crud-app/src/components/TaskForm.js
@@ -1,13 +1,32 @@
 //rfce
-import {useSelector} from 'react-redux'
+//import {useSelector} from 'react-redux'
+import {useState} from 'react'
 
 function TaskForm() {
-    console.log("TaskForm");
+  const [task, setTask] = useState({
+    title: '',
+    description: ''
+  })
 
-    const stateTasks= useSelector(state => state.tasks);
-    console.log(stateTasks);
+  const handleChange = e => {
+    //console.log(e.target.name, e.target.value);
+    setTask({
+      ...task,
+      [e.target.name]: e.target.value,
+    })
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log(task);
+  }
+
   return (
-    <div>TaskForm</div>
+    <form onSubmit={handleSubmit}>
+      <input name='title' type='text' placeholder='title' onChange={handleChange}/>
+      <textarea name="description" placeholder='description' cols="30" rows="10" onChange={handleChange}></textarea>
+      <button>Save</button>
+    </form>
   )
 }
 
